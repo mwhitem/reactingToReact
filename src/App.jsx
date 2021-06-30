de787fac9bee8b30ad519475f91384ac7dc2f4ad
Madison White <mvwhite32@gmail.com>
2021-06-30 11:08:23 -0500

    finished reacting to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Greeter from './components/Greeter';
 
 const App = () => {
+    const [loaded, setLoaded] = useState(false);
     const [username, setUsername] = useState('');
-    return (
-        <div>
-        <Greeter name='madison' phrase='hello its'/>
-        <Greeter name='spongebob' phrase='aloha its'/>
-        <Greeter name='kylo ren' phrase='good evening its'/>
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-<p>You are logging in as: {username}</p>
-        </div>
-    );
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaded(true)
+        }, 3000);
+        }, []);
 
+            if (loaded) {
+                return (
+                    <div>
+            <Greeter name='madison' phrase='hello its'/>
+            <Greeter name='spongebob' phrase='aloha its'/>
+            <Greeter name='kylo ren' phrase='good evening its'/>
+            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+    <p>You are logging in as: {username}</p>
+            </div>
+        );
+    } else {
+        return (
+            <>
+        <h1>page is loading </h1>
+        <button onClick={() => {setLoaded(true)}}> click to load </button>
+        </>
+        )
+    }
+    
 };
 
 export default App; 
